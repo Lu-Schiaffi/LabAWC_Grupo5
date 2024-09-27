1f0e30dbb809e53547cb9b2ca939628ce1316a35
delru <ulidebonis198@gmail.com>
2024-09-27 20:18:41 -0300

    color botones carrito

--- src/app/createCartCards.js
+++ src/app/createCartCards.js
@@ -29,10 +29,10 @@ export function createCartCards(objLocalStorage) {
                 <div class="card-body d-flex flex-column justify-content-center">
                     <h5 class="card-title">${displayTitle}</h5>
                     <div class="d-flex align-items-center">
-                        <button type="button" class="btn btn-outline-dark btn-sm me-2" id="buttonLess-${product.id}-cart">-</button>
+                        <button type="button" class="btn btn-outline-dark text-bg-secondary btn-sm me-2" id="buttonLess-${product.id}-cart">-</button>
                         <span class="fw-bold me-2">Cantidad: ${product.quantity}</span>
-                        <button type="button" class="btn btn-outline-dark btn-sm me-3" id="buttonAdd-${product.id}-cart">+</button>
-                        <button type="button" class="btn btn-outline-dark btn-sm" id="buttonDelete-${product.id}-cart">
+                        <button type="button" class="btn btn-outline-dark text-bg-secondary btn-sm me-3" id="buttonAdd-${product.id}-cart">+</button>
+                        <button type="button" class="btn btn-outline-dark text-bg-secondary btn-sm" id="buttonDelete-${product.id}-cart">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
                                 <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
